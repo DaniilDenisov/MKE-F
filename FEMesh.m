29a3ebd863294d0c9bc55436159de9452ee74466
Даниил Денисов <daniil-denisov@ya.ru>
2017-12-07 22:53:59 +0300

    Доработан FEMesh. Новый формат входного файла.

--- FEMesh.m
+++ FEMesh.m
@@ -2,132 +2,54 @@
 % Copyright 2017 Daniil S. Denisov
 classdef FEMesh < handle
     properties (Access = public)
-        numberOfElems;
-        numberOfNodes;
-        numberOfBCs;
+        % Число элементов.
+        numberOfElems=0;
+        % Число узлов.
+        numberOfNodes=0;
+        % Число ГУ с приложенной силой.
+        numberOfForceBCs=0;
+        % Число гу заделки.
+        numberOfFixBCs=0;
+        % Число степеней свободы на один узел.
         dofPerNode;
-        % Поэлементная м-ца соответствия. Узловые СС - к глоб номерам СС.
-        iM;
         % Поузловая матрица соответствия.
         iMnod;
+        % Массив элементов (пользовательский класс).
         allMeshElems;
+        % Массив узлов.
         allNodes;
-        allBCs;
+        % Массив ГУ заделки.
+        allFixBCs;
+        % Массив ГУ приложенных сил.
+        allForceBCs;
     end
     methods
         % Чтение файла сетки (конструктор).
         function obj = FEMesh(filename)
-            % Чтение строки с размерами массивов.
-            totalLine = dlmread(filename,',',[0 0 0 2]);
-            % Разбор строки с размерами. Вывод количеств "всего".
-            obj.numberOfElems = totalLine(1);
-            obj.numberOfNodes = totalLine(2);
-            obj.numberOfBCs = totalLine(3);
-            fprintf('Els:%d Nodes:%d BCs:%d\n', obj.numberOfElems,...
-                obj.numberOfNodes, obj.numberOfBCs);
-            % Чтение строк данных в соответствии с размерами массивов.
-            % Тип эл-та, номера узлов в элементах и данные элементов.
-            allElemsTmp = dlmread(filename,',',...
-                [1 0 obj.numberOfElems 6]);
-            % Координаты узлов.
-            obj.allNodes = dlmread(filename,',',...
-                [obj.numberOfElems+1 0 ...
-                obj.numberOfNodes+obj.numberOfElems 2]);
-            % Граничные условия.
-            obj.allBCs = dlmread(filename,',',...
-                [obj.numberOfNodes+obj.numberOfElems+1 0 ...
-                obj.numberOfNodes+obj.numberOfElems+obj.numberOfBCs 5]);
-            % Преаллокация полного набора пустых КЭ.
-            elemArrTmp(obj.numberOfElems,1) = Truss2DElement();
-            obj.allMeshElems = elemArrTmp;
-            % Наполнение пустых КЭ данными.
-            for i=1:obj.numberOfElems
-                % Выбрать строку с данными i-го элемента.
-                currElem = allElemsTmp(i,:);
-                % Взять тип элемента.
-                currType = currElem(1);
-                % Взять номера узлов. Два номера узлов.
-                currNode1 = currElem(2);
-                currNode2 = currElem(3);
-                % Поля данных: площадь, модуль Юнга, плотность,
-                % момент инерции поперечного сечения.
-                currArea = currElem(4);
-                currEmod = currElem(5);
-                currRho = currElem(6);
-                currI = currElem(7);   % Для элемента фермы =0.
-                % Координаты узлов.
-                currNode1_XYZ = [obj.allNodes(currNode1,1) ...
-                    obj.allNodes(currNode1,2) ...
-                    obj.allNodes(currNode1,3)];
-                currNode2_XYZ = [obj.allNodes(currNode2,1) ...
-                    obj.allNodes(currNode2,2) ...
-                    obj.allNodes(currNode2,3)];
-                % Наполнение объекта элемента в объекте FEМеsh и
-                % заполнение строки таблицы соответствия iM.
-                switch currType
-                    % Для элемента типа 112.
-                    case 112
-                        obj.allMeshElems(i).SetupElement(...
-                            [currNode1_XYZ; currNode2_XYZ],...
-                            [currNode1,currNode2], ...
-                            [currArea, currEmod, currRho]);
-                        % Для матрицы соответствия эл-та тип 112.
-                        obj.dofPerNode = 2;
-                        % Взять номера узлов.
-                        nodeNums = obj.allMeshElems(i).GetNodesNums;
-                        % Вычислить глобальные индексы степеней свободы.
-                        GD1 = (nodeNums(1)-1)*(obj.dofPerNode)+1;
-                        GD2 = GD1+1;
-                        GD3 = (nodeNums(2)-1)*(obj.dofPerNode)+1;
-                        GD4 = GD3+1;
-                        obj.iM(i,1) = GD1;
-                        obj.iM(i,2) = GD2;
-                        obj.iM(i,3) = GD3;
-                        obj.iM(i,4) = GD4;
-                        % Создание (перезапись) записи в поузловой матрице
-                        % соответствия iMnod. Не эффективно.
-                        % Присваиваются повторно. Надо доработать.
-                        obj.iMnod(nodeNums(1),1) = GD1;
-                        obj.iMnod(nodeNums(1),2) = GD2;
-                        obj.iMnod(nodeNums(2),1) = GD3;
-                        obj.iMnod(nodeNums(2),2) = GD4;
-                    case 113
-                        obj.allMeshElems(i).SetupElement(...
-                            [currNode1_XYZ; currNode2_XYZ],...
-                            [currNode1,currNode2], ...
-                            [currArea, currEmod, currRho, currI]);
-                        % Для матрицы соответствия эл-та тип 112.
-                        obj.dofPerNode = 3;
-                        % Взять номера узлов.
-                        nodeNums = obj.allMeshElems(i).GetNodesNums;
-                        % Вычислить глобальные индексы степеней свободы.
-                        GD1 = (nodeNums(1)-1)*(obj.dofPerNode)+1;
-                        GD2 = GD1+1;
-                        GD3 = GD2+1;
-                        GD4 = (nodeNums(2)-1)*(obj.dofPerNode)+1;
-                        GD5 = GD4+1;
-                        GD6 = GD5+1;
-                        % Заполнение элементной матрицы соответствия.
-                        obj.iM(i,1) = GD1;
-                        obj.iM(i,2) = GD2;
-                        obj.iM(i,3) = GD3;
-                        obj.iM(i,4) = GD4;
-                        obj.iM(i,5) = GD5;
-                        obj.iM(i,6) = GD6;
-                        % Создание (перезапись) записи в поузловой матрице
-                        % соответствия iMnod. Не эффективно.
-                        % Если узел уже был в другом эл-те, то
-                        % тут присваиваются повторно. Надо доработать.
-                        obj.iMnod(nodeNums(1),1) = GD1;
-                        obj.iMnod(nodeNums(1),2) = GD2;
-                        obj.iMnod(nodeNums(1),3) = GD3;
-                        obj.iMnod(nodeNums(2),1) = GD4;
-                        obj.iMnod(nodeNums(2),2) = GD5;
-                        obj.iMnod(nodeNums(2),3) = GD6;
+            % Создание дескриптора файла. -1 если файла нет.
+            fid = fopen(filename,'r');
+            % Макс. число блоков = 10.
+            for bn=1:10
+                % Чтение строки.
+                line = fgetl(fid);
+                % Первая строка должна быть маркером. Решение по типу маркера.
+                switch line
+                    case 'nodes'
+                        obj.readNodes(fid);
+                    case 'elems_112'
+                        obj.readElems112(fid);
+                    case 'bcforce_stat'
+                        obj.readBcForcesStat(fid);
+                    case 'bcfix'
+                        obj.readBcFix(fid);
+                    case -1
+                        break
                     otherwise
-                        disp('FEMesh: Unknown element type.');
+                        warning('Unexpected line marker in task file.');
                 end
             end
+            % Закрытие файла.
+            fclose(fid);
         end
         % Изображение 2D сетки на графике.
         function Plot2DMesh(this)
@@ -173,7 +95,7 @@ classdef FEMesh < handle
         function DispIM(this)
             format shortG;
             disp('DOF distribution matrix:');
-            disp(this.iM);
+            disp(this.iMnod);
             format compact;
         end
         % Печать узлов из элементов сетки.
@@ -185,6 +107,80 @@ classdef FEMesh < handle
                 disp(nn);
             end
             format compact;
+        end
+        % Печать элементов.
+        function DispElems(this)
+            for i=1:this.numberOfElems
+               this.allMeshElems(i).Disp();
+            end
+        end
+        % -------------------- Блоки данных (чтение) --------------------
+        % Чтение блока данных об узлах.
+        function readNodes(this,fid)
+            % В начале блока должен быть размер.
+            line = fgetl(fid);
+            nNum = sscanf(line,'%d');
+            this.numberOfNodes = nNum;
+            % Чтение строк и сохранение поле узлов.
+            for i=1:nNum
+                this.allNodes(i,:) = sscanf(fgetl(fid),'%f,%f,%f');
+            end
+        end
+        % Чтение блока данных об элементах 112.
+        % Внимание! Массив узлов должен быть заполнен.
+        function readElems112(this, fid)
+            % В начале блока должен быть размер.
+            line = fgetl(fid);
+            elNum = sscanf(line,'%d');
+            this.numberOfElems = elNum;
+            % Преаллокация набора пустых КЭ типа 112 (Truss2DElement).
+            elemArrTmp(elNum,1) = Truss2DElement();
+            this.allMeshElems = elemArrTmp;
+            % Для элемента 112 должно быть 2 СС в узле.
+            this.dofPerNode = 2;
+            % Далее - данные. В соответствии с размером.
+            for i=1:elNum
+                line = fgetl(fid);
+                splitElemLine = strsplit(line,',');
+                % Чтение типа элемента пропущено, т.к. реализованы маркеры.
+                % Чтение со второй позиции. Номера узлов.
+                elNodes = str2double(splitElemLine(2:3));
+                elData = str2double(splitElemLine(4:6));
+                elNode1Coords = this.allNodes(elNodes(1),:);
+                elNode2Coords = this.allNodes(elNodes(2),:);
+                this.allMeshElems(i).SetupElement(...
+                            [elNode1Coords; elNode2Coords],...
+                            elNodes, ...
+                            elData);
+            end
+            % Можем создать матрицу соответствия, т.к. есть dofPerNode.
+            for i=1:this.numberOfNodes
+                this.iMnod(i,:) = [(i*this.dofPerNode)-1 i*this.dofPerNode];
+            end
+        end
+        % Чтение блока данных о граничных условиях заделки.
+        function readBcFix(this, fid)
+            % В начале блока должен быть размер.
+            line = fgetl(fid);
+            bcFixNum = sscanf(line,'%d');
+            this.numberOfFixBCs = this.numberOfFixBCs + bcFixNum;
+            % Чтение строк и сохранение поле узлов.
+            for i=1:bcFixNum
+                this.allFixBCs(i,:) = sscanf(fgetl(fid),...
+                    '%f,%f,%f,%f,%f,%f');
+            end
+        end
+        % Чтение блока данных о граничных условиях приложенных сил.
+        function readBcForcesStat(this, fid)
+            % В начале блока должен быть размер.
+            line = fgetl(fid);
+            bcForceNum = sscanf(line,'%d');
+            this.numberOfForceBCs = this.numberOfForceBCs + bcForceNum;
+            % Чтение строк и сохранение поле узлов.
+            for i=1:bcForceNum
+                this.allForceBCs(i,:) = sscanf(fgetl(fid),...
+                    '%f,%f,%f,%f,%f');
+            end
         end
     end
 end
